cbcd3521a5d5dcfff0c66d7a3d7ef7ede5acb5e2
yuriel <yuriel3183@gmail.com>
2016-11-28 01:03:18 +0800

    fixed host

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello(path):
+def hello():
     return "Hello Exako!"
 
 if __name__ == "__main__":
-    app.run(port=80)
+    app.run(host="0.0.0.0", port=80)
