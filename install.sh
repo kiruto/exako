a9ce7054c6e1c090f3ac4fff2213c9bb7eed3dc2
yuriel <yuriel3183@gmail.com>
2016-12-19 15:22:34 +0800

    added installation

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 # require venv path for python 3.5.2
-# require sentry_venv path for python 2.7.10
+virtualenv venv -p python3
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Init maria database
+echo Please input password for mysql root user:
+mysql -u user -p < install.sql
---
+++ install.sql
@@ -0,0 +1,4 @@
+use mysql;
+CREATE DATABASE IF NOT EXISTS exako;
+CREATE USER exako@localhost IDENTIFIED BY 'd722772d5e7dba3736d9d6f0af115722';
+GRANT ALL PRIVILEGES ON exako.* TO exako@localhost;
